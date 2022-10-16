3071d6a65aaff875d1567f683763ae5a7c3591f2
David Castrillón <97773641+Davedcod@users.noreply.github.com>
2022-10-16 15:46:21 -0500

    5

---
+++ src/src/main/java/futbol/Portero.java
@@ -0,0 +1,31 @@
+package futbol;
+
+public class Portero extends Futbolista {
+    public short golesRecibidos;
+    public byte dorsal;
+
+    public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+        super(nombre, edad, "Portero");
+        this.golesRecibidos = golesRecibidos;
+        this.dorsal = dorsal;
+    }
+
+    @Override
+    public boolean jugarConLasManos() {
+        return true;
+    }
+
+    @Override
+    public int compareTo(Futbolista futbolista) {
+        return Math.abs(this.golesRecibidos - ((Portero) futbolista).golesRecibidos);
+    }
+
+    @Override
+    public String toString() {
+        return "El futbolista \"+<" + getNombre() +">+\" tiene \"+<" + getEdad() + ">+\", y juega de \"+<" +
+                getPosicion() + ">+\" con el dorsal \"+<" + dorsal + ">+\". Le han marcado \"+<" + golesRecibidos +
+                ">+\"";
+    }
+
+
+}
